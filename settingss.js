29a47f7049ce32a6d0d23806121341f56fe22044
iamthesurya7559 <psuryarajpc@gmail.com>
2025-07-24 08:19:08 +0530

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: " ",  // add your Session Id make sure it starts with malvin~
-  OWNER_NUMBER: "263714757857", // PUT OWNER NUMBER HERE
+  SESSION_ID: " ",  // add malvin~0i8z0BIK#p15RTIa1DqalEDS7jU51amkUkFZZPdt8N7oVC-EA9OAyour Session Id make sure it starts with malvin~
+  OWNER_NUMBER: "263714757857", // 7559941711
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
